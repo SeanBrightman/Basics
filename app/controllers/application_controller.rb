a194d1e5b3d0c486b85b531cc1077de54a04f1f8
Seam Brightman <sbrightm@ggc.edu>
2015-04-23 18:47:43 +0000

    init

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,16 +6,15 @@ protect_from_forgery with: :exception
 private
 
   def current_user
-    @current_user ||= User.find(session[:user_id])
+    @current_user ||= User.find(session[:user_id]) 
     if session[:user_id]
     end
   helper_method :current_user
+  end
   
   def authorize
     redirect_to new_session_path, alert: "Not authorized"
     if current_user.nil?
     end
-  end
-  
   end
 end
